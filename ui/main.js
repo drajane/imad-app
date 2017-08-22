8419988435cb6768919511ffeebb523a60d9ade9
drajane <30800940+drajane@users.noreply.github.com>
2017-08-22 17:18:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,3 @@
 console.log('Loaded!');
 
-var element = document.getElementById('main-text');
-element.innerHTML='New value';
 
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight, 50)
-};
