ee7c4a3dfcb50a41c8c771ee30dd74b643277474
drajane <30800940+drajane@users.noreply.github.com>
2017-08-22 16:46:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,7 @@ var element = document.getElementById('main-text');
 element.innerHTML='New value';
 
 var img = document.getElementById('madi');
+var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
