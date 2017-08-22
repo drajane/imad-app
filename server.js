289d344eaef16f9d6b367ce3f99b21038299a226
Divya Rajaneganth <divya.rajaneganth@gmail.com>
2017-08-22 12:58:49 +0530

    Adding article-one

--- server.js
+++ server.js
@@ -5,6 +5,53 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article One | Divya Rajaneganth',
+    heading: 'Article One',
+    date: 'Aug 22, 2017',
+    content: `
+        <p>
+            This is content for my first article
+        </p>
+        `
+};
+
+function createTemplate(data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet"/>
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>${heading}</h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    <p>
+                        ${content}
+                    </p>
+                </div>
+            </div>
+        </body>
+    </html>
+    `
+    ;
+    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -15,6 +62,10 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('/article-one', function (req, res) {
+  res.send(createTemplate(articleOne));
 });
 
 
